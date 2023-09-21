be122399eae5b30e2e5f004368a5869966f2c3df
bradyhodge <bradyhodge1@gmail.com>
2023-09-20 18:44:17 -0600

    Finished C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    { 
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = int.MaxValue;
+        int guessCount = 0;
+        bool keepPlaying = true;
+
+        while (keepPlaying) {
+            do {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+                if (guess > magicNumber) {
+                    Console.WriteLine("Lower");
+                } else if (guess < magicNumber) {
+                    Console.WriteLine("Higher");
+                } else {
+                    Console.WriteLine("You guessed it!");
+                    if (guessCount == 1) {
+                        Console.WriteLine($"It only took you 1 guess? Wow!");
+                    } else {
+                        Console.WriteLine($"It took you {guessCount} guesses.");
+                        Console.Write("Would you like to play again? (y/n) ");
+                        string playAgain = Console.ReadLine().ToLower();
+                        if (playAgain == "n") {
+                            keepPlaying = false;
+                        } else {
+                            magicNumber = randomGenerator.Next(1, 101);
+                            guessCount = 0;
+                        }
+                    }
+                    
+                }
+            } while (guess != magicNumber);
+        }
+        
+            }
 }
