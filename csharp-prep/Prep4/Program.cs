524193ef9da8110d4ce4b6987752828e6c7a1060
bradyhodge <bradyhodge1@gmail.com>
2023-09-20 19:42:08 -0600

    Finished C# Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = 0;
+        Console.WriteLine("Enter a list of umbers, type 0 when finished.");
+        do {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0){
+                numbers.Add(number);
+            }
+            
+        } while (number != 0);
+        Console.WriteLine($"The sum of the numbers is {numbers.Sum()}");
+        Console.WriteLine($"The average of the numbers is {Math.Round(numbers.Average(), 3)}");
+        Console.WriteLine($"The largest number is {numbers.Max()}");
+        Console.WriteLine($"The smallest positive number is {numbers.Where(n => n > 0).Min()}");
+        numbers.Sort();
+        Console.WriteLine("The sorted list is: ");
+        foreach (int num in numbers) {
+            Console.WriteLine($"{num} ");
+        }
+        
     }
 }
