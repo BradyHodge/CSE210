34079dff03fb80701e110e641340971a7acd6041
Brady Hodge <bradyhodge1@gmail.com>
2023-10-19 15:22:40 -0600

    working on main/Program class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,122 @@
-using System;
+// Purpose:
+//     Initialize the program. 
+//     Display the main menu to the user. 
+//     Handle user input and direct the flow of the program.
+//     Create instances of other classes as needed.
+//     Terminate the program.
 
-class Program
+
+// Behaviors:
+//     displayMenu(): Display the main menu options to the user.
+//     handleUserInput(): Take user input and call the appropriate methods or create instances of other classes based on the input.
+//     initializeProgram(): Set up any initial configurations or objects needed for the program to run.
+//     terminateProgram(): End the program gracefully, ensuring any necessary cleanup is done.
+
+// Attributes: 
+//    Scripture scriptureInstance: An instance of the Scripture class to manage the scripture-related functionalities.
+//    bool isRunning: A boolean variable to keep track of whether the program is running or not.
+
+// Data Types: 
+//    isRunning: bool
+
+// Default constructor to initialize the program.
+// Importing required namespaces
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+// Declaring the Program class
+public class Program 
+{ 
+    // Declaring a private field of type Scripture
+    private Scripture scriptureInstance;
+    // Declaring a private boolean field to check if the program is running
+    private bool isRunning;
+
+    // Default constructor for the Program class
+    public Program() 
+    { 
+        // Setting the isRunning field to true
+        isRunning = true;
+        // Calling the initializeProgram method
+        initializeProgram();
+    }
+
+    // Method to initialize the program
+    private void initializeProgram() 
 {
-    static void Main(string[] args)
+    
+}
+
+    // Method to display the menu to the user
+    private void displayMenu() 
+    { 
+        // Clearing the console
+        Console.Clear();
+        // Printing the scriptureInstance to the console
+        Console.WriteLine(scriptureInstance);
+        // Printing instructions to the user
+        Console.WriteLine("\nPress ENTER to hide words or type 'quit' to exit.");
+    }
+
+    // Method to handle the user's input
+    private void handleUserInput() 
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Reading the user's input from the console
+        var input = Console.ReadLine();
+        // Checking if the user typed "quit" (case insensitive)
+        if (input.ToLower() == "quit") 
+        {
+            // Setting isRunning to false to stop the program
+            isRunning = false;
+        } 
+        else 
+        {
+            // Calling the HideRandomWords method of the scriptureInstance
+            scriptureInstance.HideRandomWords();
+            // Checking if all words are hidden
+            if (scriptureInstance.AreAllWordsHidden()) 
+            {
+                // Clearing the console
+                Console.Clear();
+                // Informing the user that all words are hidden
+                Console.WriteLine("All words are hidden. Press any key to exit.");
+                // Waiting for a key press from the user
+                Console.ReadKey();
+                // Setting isRunning to false to stop the program
+                isRunning = false;
+            }
+        }
+    }
+
+    // Method to terminate the program
+    private void terminateProgram() 
+    {
+        // Printing a goodbye message to the user
+        Console.WriteLine("Goodbye!");
+    }
+
+    // Method to run the program
+    public void Run() 
+    {
+        // Running the program as long as isRunning is true
+        while (isRunning) 
+        {
+            displayMenu();
+            handleUserInput();
+        }
+        // Calling the terminateProgram method when the program stops
+        terminateProgram();
+    }
+
+    // Main method, the entry point of the program
+    public static void Main(string[] args) 
+    {
+        // Creating a new instance of the Program class
+        Program program = new Program();
+        // Running the program
+        program.Run();
     }
 }
