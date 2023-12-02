bcef5ebd34ff41a051e436c03c646ac44c896bc0
bradyhodge <bradyhodge1@gmail.com>
2023-12-02 14:17:43 -0700

    EA SPORTS ITS IN THE GAME

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address1 = new Address("123 Main St", "Arimo", "ID", "USA");
+        Address address2 = new Address("456 Market Rd", "Richmond", "BC", "Canada");
+
+        Customer customer1 = new Customer("John Doe", address1);
+        Customer customer2 = new Customer("Jane Smith", address2);
+
+        Product product1 = new Product("Widget", "W123", 10.99m, 2);
+        Product product2 = new Product("Gadget", "G456", 15.50m, 1);
+        Product product3 = new Product("Thingamajig", "T789", 5.75m, 3);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
+        Console.WriteLine("Order 1:");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: {order1.CalculateTotalCost():C2}\n");
+
+        Console.WriteLine("Order 2:");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: {order2.CalculateTotalCost():C2}\n");
     }
 }
