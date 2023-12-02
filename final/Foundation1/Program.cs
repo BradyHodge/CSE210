04694ad58afce574285323edabca4d05b9e3efcc
bradyhodge <bradyhodge1@gmail.com>
2023-12-02 13:07:59 -0700

    Starting main

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video("How to Learn Programming", "CodeAcademy", 300);
+        Video video2 = new Video("Understanding OOP Concepts", "TechTutorials", 600);
+
+        video1.AddComment(new Comment("Alice", "Great explanation!"));
+        video1.AddComment(new Comment("Bob", "This really helped, thanks!"));
+
+        video2.AddComment(new Comment("Charlie", "Very insightful."));
+        video2.AddComment(new Comment("Dave", "I appreciate the detailed examples."));
+
+        var videos = new Video[] { video1, video2 };
+
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+            Console.WriteLine();
+        }
     }
 }
