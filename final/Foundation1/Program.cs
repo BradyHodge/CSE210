dc71f32a2114d94b552e63b8038ea35a66be341b
bradyhodge <bradyhodge1@gmail.com>
2023-12-02 13:50:24 -0700

    Update video titles and comments

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Video video1 = new Video("How to Learn Programming", "CodeAcademy", 300);
-        Video video2 = new Video("Understanding OOP Concepts", "TechTutorials", 600);
+        Video video1 = new Video("Exploring the Cosmos", "AstroVlogs", 1200);
+        Video video2 = new Video("The Art of Cooking", "ChefJulia", 900);
+        Video video3 = new Video("Mystery of the Ocean Depths", "SeaExplorer", 1500);
+        Video video4 = new Video("Journey Through the History", "HistoricFacts", 1800);
 
-        video1.AddComment(new Comment("Alice", "Great explanation!"));
-        video1.AddComment(new Comment("Bob", "This really helped, thanks!"));
+        video1.AddComment(new Comment("Alex", "Absolutely fascinating!"));
+        video1.AddComment(new Comment("Taylor", "Can't wait for the next episode."));
+        video1.AddComment(new Comment("Jordan", "This is amazing!"));
 
-        video2.AddComment(new Comment("Charlie", "Very insightful."));
-        video2.AddComment(new Comment("Dave", "I appreciate the detailed examples."));
+        video2.AddComment(new Comment("Morgan", "Delicious recipes."));
+        video2.AddComment(new Comment("Chris", "Your cooking tips are the best!"));
+        video2.AddComment(new Comment("Pat", "Love your presentation style!"));
 
-        var videos = new Video[] { video1, video2 };
+        video3.AddComment(new Comment("Casey", "So intriguing!"));
+        video3.AddComment(new Comment("Riley", "Learned so much about marine life."));
+        video3.AddComment(new Comment("Sam", "Great exploration video!"));
+
+        video4.AddComment(new Comment("Jamie", "History brought to life."));
+        video4.AddComment(new Comment("Drew", "Very informative and engaging."));
+        video4.AddComment(new Comment("Robin", "Your storytelling is fantastic!"));
+
+        List<Video> videos = new List<Video> { video1, video2, video3, video4 };
 
         foreach (var video in videos)
         {
