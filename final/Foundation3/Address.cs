d2caf783052d2dc03928bd0d1afb76ae7d1d5849
bradyhodge <bradyhodge1@gmail.com>
2023-12-09 15:09:32 -0700

    address?

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,44 @@
+public class Address
+{
+    private string street;
+    private string city;
+    private string state;
+    private string country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public string Street
+    {
+        get { return street; }
+        set { street = value; }
+    }
+
+    public string City
+    {
+        get { return city; }
+        set { city = value; }
+    }
+
+    public string State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public string Country
+    {
+        get { return country; }
+        set { country = value; }
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{street}, {city}, {state}, {country}";
+    }
+}
