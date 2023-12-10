bfd9a0573db50bdc978a97953efdcee5d24327a4
bradyhodge <bradyhodge1@gmail.com>
2023-12-09 18:20:50 -0700

    everything works?

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+    
+        List<Activity> activities = new List<Activity>();
+
+    
+        Running run = new Running(new DateTime(2023, 11, 24), 30, 3.0);
+        Cycling cycle = new Cycling(new DateTime(2023, 11, 24), 45, 15.0);
+        Swimming swim = new Swimming(new DateTime(2023, 11, 24), 60, 20);
+
+    
+        activities.Add(run);
+        activities.Add(cycle);
+        activities.Add(swim);
+
+    
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
